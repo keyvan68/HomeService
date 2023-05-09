075ea7ea612ebfd79ee3b00afc6d8482f7fa0fa3
keyvan68 <keyvanhafezy1368@gmail.com>
2023-05-09 21:03:44 +0430

    Add project files.

---
+++ src/Domain/HomeService.Domain.Core/Entities/ServiceCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeService.Domain.Core.Entities
+{
+    
+    public partial class ServiceCategory
+    {
+        public int Id { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public DateTime? CreatedAt { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public virtual ICollection<Service> Services { get; set; } = new List<Service>();
+    }
+}
---
+++ src/EndPoints/HomeService.EndPoints.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomeService.EndPoints.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
